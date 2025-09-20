6e4147258d76325d56636fbd651b47bf6d868f05
SowvikSuvo <skarmoker44@gmail.com>
2025-09-20 11:25:54 +0600

    Added Name, population, capital, continents, region on this project

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const Country = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Country = ({ country }) => {
+  console.log(country.region.region);
+  return (
+    <div>
+      <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+      <h3>Name: {country.name.common} </h3>
+      <p>Population: {country.population.population}</p>
+      <p>capital: {country.capital.capital} </p>
+      <p>continents: {country.continents.continents} </p>
+      <p>Region: {country.region.region} </p>
+    </div>
+  );
 };
 
 export default Country;
